6a9eff49c08f2d99f8f29a75fe464a591d42595d
atuffa <tuffajr@gmail.com>
2018-03-06 17:14:34 -0600

    jquery code buyilding phase 1

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+$(function(){
+    
+    let randomlyPickedNumber = Math.floor((Math.random() * 11) + 30);
+    document.getElementById("randomPickedNumber").innerHTML = randomlyPickedNumber ;
+    // function randomNumber() {
+    //     randomlyPickedNumber = Math.floor((Math.random() * 40) + 30);
+    //     document.getElementById("randomPickedNumber").innerHTML = randomlyPickedNumber ;
+    // }
+    
+
+
+ 
+ });
